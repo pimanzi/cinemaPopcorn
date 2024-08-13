c3d9879abc599136d787d8fa29ce96e7072ca715
kimanziplacide <p.imanzi@alustudent.com>
2024-08-13 14:18:19 +0200

    fixing fetching issues

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -58,7 +58,7 @@ export function MovieDetails({
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${APIKEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${APIKEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
